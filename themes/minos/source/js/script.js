eedf6108b8708a7a13f382647af991c34f9d1c58
brchen777 <eric790620@gmail.com>
2019-10-27 22:48:13 +0800

    Site updated: 2019-10-27 22:48:13

--- themes/minos/source/js/script.js
+++ themes/minos/source/js/script.js
@@ -1,17 +1,17 @@
-(function ($) {
-    $('.navbar-burger').click(function () {
-        $(this).toggleClass('is-active');
-        $('.navbar-main .navbar-start').toggleClass('is-active');
-        $('.navbar-main .navbar-end').toggleClass('is-active');
+(function($) {
+    $(".navbar-burger").click(function() {
+        $(this).toggleClass("is-active");
+        $(".navbar-main .navbar-start").toggleClass("is-active");
+        $(".navbar-main .navbar-end").toggleClass("is-active");
     });
 
     // Hide Header on on scroll down
     var didScroll;
     var lastScrollTop = 0;
     var delta = 5;
-    var navbarHeight = $('.navbar-main').outerHeight();
+    var navbarHeight = $(".navbar-main").outerHeight();
 
-    $(window).scroll(function(event){
+    $(window).scroll(function(event) {
         didScroll = true;
     });
 
@@ -26,7 +26,7 @@
         var st = $(this).scrollTop();
 
         // Make sure they scroll more than delta
-        if(Math.abs(lastScrollTop - st) <= delta) {
+        if (Math.abs(lastScrollTop - st) <= delta) {
             return;
         }
 
@@ -35,22 +35,22 @@
         if (st > lastScrollTop && st > navbarHeight) {
             var posY = Math.min(st, navbarHeight);
             // Scroll Down
-            $('.navbar-main').css({
-                '-webkit-transform' : 'translateY(-' + posY + 'px)',
-                '-moz-transform'    : 'translateY(-' + posY + 'px)',
-                '-ms-transform'     : 'translateY(-' + posY + 'px)',
-                '-o-transform'      : 'translateY(-' + posY + 'px)',
-                'transform'         : 'translateY(-' + posY + 'px)'
+            $(".navbar-main").css({
+                "-webkit-transform": "translateY(-" + posY + "px)",
+                "-moz-transform": "translateY(-" + posY + "px)",
+                "-ms-transform": "translateY(-" + posY + "px)",
+                "-o-transform": "translateY(-" + posY + "px)",
+                transform: "translateY(-" + posY + "px)"
             });
         } else {
             // Scroll Up
-            if(st + $(window).height() < $(document).height()) {
-                $('.navbar-main').css({
-                    '-webkit-transform' : 'translateY(0px)',
-                    '-moz-transform'    : 'translateY(0px)',
-                    '-ms-transform'     : 'translateY(0px)',
-                    '-o-transform'      : 'translateY(0px)',
-                    'transform'         : 'translateY(0px)'
+            if (st + $(window).height() < $(document).height()) {
+                $(".navbar-main").css({
+                    "-webkit-transform": "translateY(0px)",
+                    "-moz-transform": "translateY(0px)",
+                    "-ms-transform": "translateY(0px)",
+                    "-o-transform": "translateY(0px)",
+                    transform: "translateY(0px)"
                 });
             }
         }
@@ -58,25 +58,42 @@
         lastScrollTop = st;
     }
 
-    $('.article.gallery img:not(".not-gallery-item")').each(function () {
+    $('.article.gallery img:not(".not-gallery-item")').each(function() {
         // wrap images with link and add caption if possible
-        if ($(this).parent('a').length === 0) {
-            $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
+        if ($(this).parent("a").length === 0) {
+            $(this).wrap('<a class="gallery-item" href="' + $(this).attr("src") + '"></a>');
             if (this.alt) {
-                $(this).after('<div class="caption">' + this.alt + '</div>');
+                $(this).after('<div class="caption">' + this.alt + "</div>");
             }
         }
     });
 
-    $('.article-entry').find('h1, h2, h3, h4, h5, h6').on('click', function () {
-        if ($(this).get(0).id) {
-            window.location.hash = $(this).get(0).id;
-        }
-    });
+    $(".article-entry")
+        .find("h1, h2, h3, h4, h5, h6")
+        .on("click", function() {
+            if ($(this).get(0).id) {
+                window.location.hash = $(this).get(0).id;
+            }
+        });
 
-    if (typeof(moment) === 'function') {
-        $('.article-meta time').each(function () {
-            $(this).text(moment($(this).attr('datetime')).fromNow());
+    if (typeof moment === "function") {
+        $(".article-meta time").each(function() {
+            $(this).text(moment($(this).attr("datetime")).fromNow());
         });
     }
+
+    // Firebase JS Code start
+    var firebaseConfig = {
+        apiKey: "AIzaSyBWGDv6N1SHujN7h3DWEeNBJwsHwM2xwN4",
+        authDomain: "blog-brchen777.firebaseapp.com",
+        databaseURL: "https://blog-brchen777.firebaseio.com",
+        projectId: "blog-brchen777",
+        storageBucket: "blog-brchen777.appspot.com",
+        messagingSenderId: "988913757048",
+        appId: "1:988913757048:web:86a715915d99572b67f68d",
+        measurementId: "G-VDSS2BTSZ8"
+    };
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+    // Firebase JS Code End
 })(jQuery);
